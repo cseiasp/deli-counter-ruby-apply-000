69ce2e6d9e72e47d875c669f7cd03b6980ae628b
concurrent-paradigm-0158 <caroline.seilern@hotmail.com>
2019-07-30 08:12:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,28 +2,30 @@
 
 def line(katz_deli)
 new_katz_deli = []
-counts = 1
   if katz_deli.count == 0
   puts "The line is currently empty."
   else
     katz_deli.each do |person|
-      new_person = "#{counts}. #{person}"
-      new_katz_deli << new_person
-      counts += 1
+      new_katz_deli << "#{new_katz_deli.count + 1}. #{person}"
     end
-  new_katz_deli_2 = new_katz_deli.join(" ")
-  puts "The line is currently: #{new_katz_deli_2}"
+  puts "The line is currently: #{new_katz_deli.join(" ")}"
 end
 end
 
 def take_a_number(katz_deli, name)
-  position = katz_deli.count + 1
-  puts "Welcome, #{name}. You are number #{position} in line."
   katz_deli << name
+  katz_deli.count + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+$Customer_Counts = 1 
+
+def take_a_number_2
+  puts "Welcome, you are ticket number #{counts}"
+  $Costumer_Counts += 1
 end
 
 def now_serving(katz_deli)
-  
   if katz_deli.count == 0 
     puts "There is nobody waiting to be served!"
   else
